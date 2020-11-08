99739fea22b0453b7301597133db249445740753
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-11-08 16:56:35 +0400

    task3

---
+++ js/task_3.js
@@ -0,0 +1,10 @@
+const getUsersWithGender = (array, gender) => array.filter((user) => gender === user.gender).map(({name}) => (name));
+
+console.log(getUsersWithGender(users, 'male'));
+
+/* [
+  "Moore Hensley",
+  "Ross Vazquez",  
+  "Carey Barr",  
+  "Blackburn Dotson"  
+] */
