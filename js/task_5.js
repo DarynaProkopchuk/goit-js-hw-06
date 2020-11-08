6fa076e4fbd033f5bbfd225c9b88fc1f448297ca
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-11-08 19:04:58 +0400

    task5

--- js/task_5.js
+++ js/task_5.js
@@ -1,20 +1,7 @@
-const getInactiveUsers = array  => array.filter(({isActive}) => !(isActive));
+const getUserWithEmail = (array, mail) => array.find(({email}) => email === mail);
 
-console.log(getInactiveUsers(users));
-/* [
-  {
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-    name: 'Moore Hensley',
-    email: 'moorehensley@indexia.com',
-    eyeColor: 'blue',
-    friends: ['Sharron Pace'],
-    isActive: false,
-    balance: 2811,
-    skills: ['ipsum', 'lorem'],
-    gender: 'male',
-    age: 37,
-  },
-  {
+console.log(getUserWithEmail(users, 'rossvazquez@xinware.com'));
+/* {
     id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
     name: 'Ross Vazquez',
     email: 'rossvazquez@xinware.com',
@@ -25,8 +12,11 @@ console.log(getInactiveUsers(users));
     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
     gender: 'male',
     age: 24,
-  },
-  {
+} */
+
+// console.log(getUserWithEmail(users, 'blackburndotson@furnigeer.com'));
+
+/* {
     id: '150b00fb-dd82-427d-9faf-2879ea87c695',
     name: 'Blackburn Dotson',
     email: 'blackburndotson@furnigeer.com',
@@ -37,5 +27,4 @@ console.log(getInactiveUsers(users));
     skills: ['non', 'amet', 'ipsum'],
     gender: 'male',
     age: 38,
-  }     
-]; */
+} */
