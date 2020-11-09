d7b121e66cfed92f0d728c644b69877a997c78dc
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-11-09 11:54:16 +0400

    task 7done

--- js/task_7.js
+++ js/task_7.js
@@ -1,11 +1,4 @@
-///
-const calculateTotalBalance = array => array.reduce((acc,users) =>({balance}) => balance + acc,0);
-const calculateTotalBalance = array => array.reduce((acc,users) => acc + ({balance}) ,0);
-const calculateTotalBalance = array => array.reduce((acc,users) =>({balance}) => balance + acc,0);
-const calculateTotalBalance = array => array.reduce((acc,balance) => acc + balance, 0);
-const calculateTotalBalance = (users) => users.reduce((acc, users) => acc + users.balance, 0); ///
 
-
-const calculateTotalBalance = array => array.reduce((acc, array) => acc + array.balance, 0);
+const calculateTotalBalance = array => array.reduce((acc, {balance}) => acc + balance, 0);
 console.log(calculateTotalBalance(users)); 
 // 20916
