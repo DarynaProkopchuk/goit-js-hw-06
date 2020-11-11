da0dee03f8b46a936cf5ce35a6f97b79acc85289
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-11-11 08:52:20 +0400

    done

--- js/task_10.js
+++ js/task_10.js
@@ -1,12 +1,12 @@
-const getSortedUniqueSkills = (array) => [...array].reduce((acc, { skills }) => acc.concat(skills), [])
-.filter(({ value }, index, array) => array.indexOf({value} === index)).sort();
-
-
-const getSortedUniqueSkills = (array) => array.map(function ({ skills }, i) {
-    return skills
-  }).reduce(function (allSkills, skills) {
-      return allSkills.concat(...skills)
-  }, []).filter((e,i,a)=>a.indexOf(e)==i).sort()
-
-
-  const getSortedUniqueSkills = (array) => array.reduce((acc, user) => acc.concat(...user.skills), []).filter((skill, index, arr) => !arr.includes(skill[index])).sort();
+const getSortedUniqueSkills = (array) => array.reduce((acc, { skills }) => acc.concat(skills), [])
+.filter((e,i,a)=>a.indexOf(e)==i).sort()
+console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
